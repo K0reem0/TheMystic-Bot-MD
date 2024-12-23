5a9c2789cafe0b43d3d8d2317bfdede581291666
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-12-23 22:03:01 +0300

    Update owner-exec2.js

--- plugins/owner-exec2.js
+++ plugins/owner-exec2.js
@@ -1,27 +1,23 @@
-import cp, {exec as _exec} from 'child_process';
-import {promisify} from 'util';
-
-const exec = promisify(_exec).bind(cp);
-const handler = async (m, {conn, isOwner, command, text, usedPrefix, args, isROwner}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_exec2
-
-  if (!isROwner) return;
-  if (global.conn.user.jid != conn.user.jid) return;
-  m.reply(tradutor.texto1);
-  let o;
+import cp, { exec as _exec } from 'child_process'
+import { promisify } from 'util'
+let exec = promisify(_exec).bind(cp)
+let handler = async (m, { conn, isOwner, command, text }) => {
+  if (conn.user.jid != conn.user.jid) return
+  m.reply('✅ running...')
+  let o
   try {
-    o = await exec(command.trimStart() + ' ' + text.trimEnd());
+    o = await exec(command.trimStart() + ' ' + text.trimEnd())
   } catch (e) {
-    o = e;
+    o = e
   } finally {
-    const {stdout, stderr} = o;
-    if (stdout.trim()) m.reply(stdout);
-    if (stderr.trim()) m.reply(stderr);
+    let { stdout, stderr } = o
+    if (stdout.trim()) m.reply(stdout)
+    if (stderr.trim()) m.reply(stderr)
   }
-};
-handler.customPrefix = /^[$]/;
-handler.command = new RegExp;
-export default handler;
+}
+handler.help = ['$']
+handler.tags = ['advanced']
+handler.customPrefix = /^[$] /
+handler.command = new RegExp()
+handler.rowner = true
+export default handler
