4172de6e97e2ebd8e216dd0e38dccfd09ad2c989
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-12-27 05:50:24 +0300

    Update dla.js

--- plugins/dla.js
+++ plugins/dla.js
@@ -1,192 +1,140 @@
-// MR. De la Comunidad para la Comunidad. Prohibida su Venta.
-// El Software se proporciona bajo los términos de la Licencia MIT, excepto que usted no puede:
-// 1. Vender, revender o arrendar el Software.
-// 2. Cobrar a otros por el acceso, la distribución o cualquier otro uso comercial del Software.
-// 3. Usar el Software como parte de un producto comercial o una oferta de servicio.
+ let handler = m => m 
+ handler.all = async function (m) { 
+ let chat = global.db.data.chats[m.chat] 
 
-import fs from "fs";
-import path, { join, basename } from "path";
-import { exec } from "child_process";
-import { promisify } from "util";  // Para hacer que exec sea promisificado.
+ if (/^آرثر$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*قلب آرثر*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-const execPromise = promisify(exec);
-const __dirname = path.resolve();
-const ytDlpTempDirectory = path.join(process.cwd(), 'src/tmp/YTDLP');
-const curlTempDirectory = path.join(process.cwd(), 'src/tmp/CURL');
-const maxDownloads = 5; // Instancias Permitidas
-let activeDownloads = 0;
-const queue = [];
+ } 
 
-const cleanCommand = (text) => text.replace(/^\.(dla)\s*/i, "").trim();
-const filterArgs = (args, filter) => args.filter(filter);
 
-const processQueue = () => {
-  if (activeDownloads >= maxDownloads) {
-    const { m } = queue[0]; 
-    m.reply(`Hay ${maxDownloads} Descargas Activas, tu Archivo Tardara un Rato.`);
-    return;
-  }
 
-  if (!queue.length) return; 
-  
-  const { m, resolve, reject } = queue.shift();
-  activeDownloads++;
 
-  handleRequest(m)
-    .then(resolve)
-    .catch(reject)
-    .finally(() => {
-      activeDownloads--;
-      processQueue(); 
-    });
-};
+if (/^سم$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*سم الله عدوك*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-let handler = (m) => {
-  return new Promise((resolve, reject) => {
-    queue.push({ m, resolve, reject });
-    processQueue();
-  });
-};
+}
 
-const handleRequest = async (m) => {
-  const command = cleanCommand(m.text.trim());
-  const args = command.split(/\s+/);
-  const urls = filterArgs(args, arg => arg.startsWith("http"));
 
-  if (args[0] === 'update') return await updateYtDlp(m);
 
-  if (args[0] === 'curl' && urls.length) {
-    const options = filterArgs(args, arg => !arg.startsWith("http") && arg !== 'curl').join(' ');
-    await Promise.all(urls.map(url => downloadWithCurl(m, url, options)));
-    return;
-  }
+if (/^كلزق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*ما اتخيلك ف صحن*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-  if (!urls.length) return await execWithoutUrl(m, args.join(' '));
+}
 
-  const options = filterArgs(args, arg => !arg.startsWith("http")).join(' ');
-  await Promise.all(urls.map(url => downloadAndSend(m, url, options)));
-};
 
-const execWithoutUrl = async (m, options) => {
-  try {
-    await m.reply(`⏳ Ejecutando...`);
-    const result = await execPromise(`yt-dlp ${options}`);
-    await m.reply(`✅ \n${result}`);
-  } catch (error) {
-    await sendErrorMessage(m, error, `yt-dlp ${options}`);
-  }
-};
+if (/^كل زق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*ما اتخيلك ف صحن*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-// YT-DLP
-const downloadAndSend = async (m, url, options) => {
-  let outputFilePathPrefix = join(ytDlpTempDirectory, `download_${Date.now()}`);
-  try {
-    await prepareDirectory(ytDlpTempDirectory);
-    await m.reply(`⏳ Descargando con YT-DLP...`);
-    await execPromise(`yt-dlp ${options} --max-filesize 1500M --yes-playlist --abort-on-error -o "${outputFilePathPrefix}_%(title)s.%(ext)s" "${url}"`);
+}
 
-    const downloadedFiles = await findDownloadedFiles(outputFilePathPrefix, ytDlpTempDirectory);
-    for (const downloadedFile of downloadedFiles) {
-      await sendDownloadedFile(m, join(ytDlpTempDirectory, downloadedFile));
-    }
-  } catch (error) {
-    await sendErrorMessage(m, error, `yt-dlp ${options} -o "${outputFilePathPrefix}_%(title)s.%(ext)s" "${url}"`);
-  } finally {
-    await cleanupFiles(ytDlpTempDirectory, outputFilePathPrefix);
-  }
-};
 
-// CURL 
-const downloadWithCurl = async (m, url, options) => {
-  const fileNameFromUrl = basename(new URL(url).pathname);
-  const outputFilePath = join(curlTempDirectory, `download_${Date.now()}_${fileNameFromUrl}`);
+if (/^كل تبن$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*حط نفسك ف صحن وتعال*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-  try {
-    await prepareDirectory(curlTempDirectory);
-    await m.reply(`⏳ Descargando con CURL...`);
-    
-    // Ejecutamos curl
-    await execPromise(`curl --max-filesize 1500000000 ${options} -o "${outputFilePath}" "${url}"`);
-    
-    if (fs.existsSync(outputFilePath)) {
-      await sendDownloadedFile(m, outputFilePath);
-    } else {
-      throw new Error(`Archivo no descargado: ${outputFilePath}`);
-    }
-  } catch (error) {
-    await sendErrorMessage(m, error, `curl ${options} -o "${outputFilePath}" "${url}"`);
-  } finally {
-    await cleanupFiles(curlTempDirectory, outputFilePath);
-  }
-};
+}
 
-// YT-DLP
-const updateYtDlp = async (m) => {
-  try {
-    await m.reply(`⏳ Actualizando YT-DLP...`);
-    await execPromise(`python3 -m pip install -U --pre "yt-dlp[default]"`);
-    await m.reply(`✅ YT-DLP actualizado (python3).`);
-  } catch (error) {
-    try {
-      const result = await execPromise(`python -m pip install -U --pre "yt-dlp[default]"`);
-      await m.reply(`✅ YT-DLP actualizado (python).\n${result}`);
-    } catch (error2) {
-      await sendErrorMessage(m, error2, "python -m pip install -U --pre yt-dlp");
-    }
-  }
-};
+if (/^دزمها$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*استح*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-const prepareDirectory = (dir) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
-};
+}
 
-const findDownloadedFiles = (filePathPrefix, directory) => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(directory, (err, files) => {
-      if (err) reject(err);
-      resolve(files.filter(file => file.startsWith(path.basename(filePathPrefix))));
-    });
-  });
-};
+ if (/^تصت$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تست*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-const sendDownloadedFile = (m, filePath) => {
-  fs.stat(filePath, (err, stats) => {
-    if (!err) {
-      conn.sendMessage(m.chat, {
-        document: { url: filePath },
-        mimetype: 'video/mp4',
-        fileName: path.basename(filePath)
-      }, { quoted: m }, () => {
-        fs.unlink(filePath, (err) => { if (err) console.error(err); });
-      });
-    } else {
-      console.error("Error al enviar el archivo:", err);
-    }
-  });
-};
+ } 
 
-const cleanupFiles = (directory, filePathPrefix) => {
-  fs.readdir(directory, (err, files) => {
-    if (err) return console.error("Error al limpiar los archivos:", err);
-    files.forEach(file => {
-      if (file.startsWith(path.basename(filePathPrefix))) {
-        fs.unlink(join(directory, file), (err) => {
-          if (err) console.error("Error al eliminar el archivo:", err);
-        });
-      }
-    });
-  });
-};
 
-const sendErrorMessage = (m, error, command) => {
-  m.reply(`❌ \n${error.message || error}\n\nComando ejecutado:\n${command}`);
-};
+ if (/^احبك$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*اقول بلا قرف*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
 
-handler.help = ['dla [OPTIONS] URL', 'dla update', 'dla curl [OPTIONS] URL'];
-handler.tags = ['tools'];
-handler.command = /^(dla)$/i;
-handler.owner = false;
+ } 
 
-export default handler;
+
+   if (/^اما يا ارثر$/i.test(m.text) ) { //sem prefixo 
+    conn.reply(m.chat, `*مب ناقصني*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+    } 
+
+
+ if (/^عمك ارثر$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*صدقت*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+ if (/^دينجي$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, ` *ماكيما عمته*, `, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+
+ if (/^بوت زق$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*امااا يطاوعك قلبك علي ؟*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+
+ if (/^نوان$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*خلك منها فاشلة*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+ if (/^السلام عليكم$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*وعليكم السلام*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^لُجين$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*الممرضة الرقله*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+ } 
+
+ if (/^تصبح على خير/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تلاقي الخير*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+ }
+
+
+
+ if (/^لوجين$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*خلك منها فاشلة*`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+
+}
+
+
+ if (/^ميسي$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*القوت العم*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^الزعيم$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*الزعيم كريس*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^برب$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*تروح وترجع ب السلامة ننتظرك*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^انس$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*رهيب بزياده*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^بياكويا$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*العم بياكويا راعي الطيب والمرجلة*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+ if (/^مين محمد عبده$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*رجل جميل و مز و اجمل رجل و معضل وغوي*`, m) //wm, null, [['Menu', '#menu']], m) botones :V 
+
+}
+
+   if (/^جينكس$/i.test(m.text) ) { //sem prefixo 
+ conn.reply(m.chat, `*عمتي جينكس*`, m) //wm, null, [['Menu', '#menu']], m) botones :V
+   }
+ return !0 } 
+ export default handler
