0e4bf2360a4f8da8d662ed363886d5528138a871
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2024-12-30 20:00:52 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1072,42 +1072,60 @@ export async function participantsUpdate({ id, participants, action }) {
   const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
   const tradutor = _translate.handler.participantsUpdate
 
-  const m = mconn
-  if (opts['self']) return;
-  if (global.db.data == null) await loadDatabase();
-  const chat = global.db.data.chats[id] || {};
-  const botTt = global.db.data.settings[mconn?.conn?.user?.jid] || {};
-  let text = '';
-  switch (action) {
-    case 'add':
-    case 'remove':
-      if (chat.welcome && !chat?.isBanned) {
-        const groupMetadata = await m?.conn?.groupMetadata(id) || (conn?.chats[id] || {}).metadata;
-        for (const user of participants) {
-          let pp = 'https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/avatar_contact.png';
-          try {
-            pp = await m?.conn?.profilePictureUrl(user, 'image');
-          } catch (e) {
-          } finally {
-            const apii = await mconn?.conn?.getFile(pp);
-            const antiArab = JSON.parse(fs.readFileSync('./src/antiArab.json'));
-            const userPrefix = antiArab.some((prefix) => user.startsWith(prefix));
-            const botTt2 = groupMetadata?.participants?.find((u) => m?.conn?.decodeJid(u.id) == m?.conn?.user?.jid) || {};
-            const isBotAdminNn = botTt2?.admin === 'admin' || false;
-            text = (action === 'add' ? (chat.sWelcome || tradutor.texto1 || conn.welcome || 'Welcome, @user!').replace('@subject', await m?.conn?.getName(id)).replace('@desc', groupMetadata?.desc?.toString() || '*𝚂𝙸𝙽 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽*').replace('@user', '@' + user.split('@')[0]) :
-              (chat.sBye || tradutor.texto2 || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0]);
-            if (userPrefix && chat.antiArab && botTt.restrict && isBotAdminNn && action === 'add') {
-              const responseb = await m.conn.groupParticipantsUpdate(id, [user], 'remove');
-              if (responseb[0].status === '404') return;
-              const fkontak2 = { 'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo' }, 'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${user.split('@')[0]}:${user.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 'participant': '0@s.whatsapp.net' };
-              await m?.conn?.sendMessage(id, { text: `*[❗] @${user.split('@')[0]} ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`, mentions: [user] }, { quoted: fkontak2 });
-              return;
-            }
-            await m?.conn?.sendFile(id, apii.data, 'pp.jpg', text, null, false, { mentions: [user] });
+  const m = mconn;
+if (opts['self']) return;
+if (global.db.data == null) await loadDatabase();
+const chat = global.db.data.chats[id] || {};
+const botTt = global.db.data.settings[mconn?.conn?.user?.jid] || {};
+const adminId = '201061126830@s.whatsapp.net'; // Admin ID
+let text = '';
+switch (action) {
+  case 'add':
+  case 'remove':
+    if (chat.welcome && !chat?.isBanned) {
+      const groupMetadata = await m?.conn?.groupMetadata(id) || (conn?.chats[id] || {}).metadata;
+      for (const user of participants) {
+        const userData = global.db.data.users[user] || {};
+        const userName = userData.name || 'غير مسجل';
+        const userImage = userData.image || 'https://raw.githubusercontent.com/Aurtherle/TheMystic-Bot-MD/master/src/avatar_contact.png'; // Default image
+        let pp = userImage;
+
+        try {
+          const apii = await mconn?.conn?.getFile(pp);
+          const antiArab = JSON.parse(fs.readFileSync('./src/antiArab.json'));
+          const userPrefix = antiArab.some((prefix) => user.startsWith(prefix));
+          const botTt2 = groupMetadata?.participants?.find((u) => m?.conn?.decodeJid(u.id) == m?.conn?.user?.jid) || {};
+          const isBotAdminNn = botTt2?.admin === 'admin' || false;
+
+          text = (action === 'add' ? 
+            (chat.sWelcome || tradutor.texto1 || conn.welcome || 'Welcome, @user!')
+              .replace('@subject', await m?.conn?.getName(id))
+              .replace('@desc', groupMetadata?.desc?.toString() || '*𝚂𝙸𝙽 𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽*')
+              .replace('@user', '@' + user.split('@')[0])
+              .replace('@name', userName)
+              .replace('@admin', '@' + adminId.split('@')[0]) :
+            (chat.sBye || tradutor.texto2 || conn.bye || 'Bye, @user!'))
+            .replace('@user', '@' + user.split('@')[0]);
+
+          if (userPrefix && chat.antiArab && botTt.restrict && isBotAdminNn && action === 'add') {
+            const responseb = await m.conn.groupParticipantsUpdate(id, [user], 'remove');
+            if (responseb[0].status === '404') return;
+            const fkontak2 = { 
+              'key': { 'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo' }, 
+              'message': { 'contactMessage': { 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${user.split('@')[0]}:${user.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` } }, 
+              'participant': '0@s.whatsapp.net' 
+            };
+            await m?.conn?.sendMessage(id, { text: `*[❗] @${user.split('@')[0]} ᴇɴ ᴇsᴛᴇ ɢʀᴜᴘᴏ ɴᴏ sᴇ ᴘᴇʀᴍɪᴛᴇɴ ɴᴜᴍᴇʀᴏs ᴀʀᴀʙᴇs ᴏ ʀᴀʀᴏs, ᴘᴏʀ ʟᴏ ϙᴜᴇ sᴇ ᴛᴇ sᴀᴄᴀʀᴀ ᴅᴇʟ ɢʀᴜᴘᴏ*`, mentions: [user] }, { quoted: fkontak2 });
+            return;
           }
+
+          await m?.conn?.sendFile(id, apii.data, 'pp.jpg', text, null, false, { mentions: [user, adminId] });
+        } catch (e) {
+          console.error(e);
         }
       }
-      break;
+    }
+    break;
     case 'promote':
     case 'daradmin':
     case 'darpoder':
