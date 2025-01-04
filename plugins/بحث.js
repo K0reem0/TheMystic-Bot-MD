6d042d1bf063d13c7ed8a5f99d6ce3378d8593d6
Abdulkareem albukari <113518431+Aurtherle@users.noreply.github.com>
2025-01-04 18:37:07 +0300

    Update بحث.js

--- plugins/بحث.js
+++ plugins/بحث.js
@@ -1,12 +1,23 @@
 import { createHash } from 'crypto';
 
-const handler = async function(m, { conn, text, usedPrefix }) {
-  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender;
+const handler = async function(m, { conn, text }) {
+  // إذا لم يتم إدخال النص، أرسل رسالة تطلب من المستخدم إدخال اللقب
+  if (!text || text.trim() === '') {
+    return m.reply(`*يرجى إدخال اللقب الذي تود البحث عنه*`);
+  }
+
+  let who = m.quoted 
+    ? m.quoted.sender 
+    : m.mentionedJid && m.mentionedJid[0] 
+      ? m.mentionedJid[0] 
+      : m.fromMe 
+        ? conn.user.jid 
+        : m.sender;
 
   const requestedName = text.trim(); // Name to check
 
   const user = global.db.data.users[who];
-  const { name } = global.db.data.users[who];
+  const { name } = user;
   const userName = name;
 
   // Check if the requested name matches any existing user's name
@@ -20,18 +31,15 @@ const handler = async function(m, { conn, text, usedPrefix }) {
       key => typeof global.db.data.users[key].name === 'string' && global.db.data.users[key].name.toLowerCase() === requestedName.toLowerCase()
     );
 
-    const usersWithSameNameInfo = usersWithSameName.map(userId => {
+    const mentionsList = usersWithSameName.map(userId => {
       const userInfo = global.db.data.users[userId];
-      const tag = `${userId.split('@')[0]}`;
-      return `${conn.getName(tag)}`;
+      return `@${userId.split('@')[0]}`; // Mention user
     });
 
-
-
     m.reply(
-      `*لقب "${requestedName}" مأخوذ بواسطة :* \n${usersWithSameNameInfo.join(
-        '\n'
-      )}`
+      `*لقب "${requestedName}" مأخوذ بواسطة :*\n${mentionsList.join('\n')}`,
+      null,
+      { mentions: usersWithSameName } // Ensure proper mentions
     );
   } else {
     // Name is not taken
