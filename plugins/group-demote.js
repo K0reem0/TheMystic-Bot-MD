be9145dcd7b5d5da95c6bef46d0f48e0cce469f6
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-07-18 20:57:22 +0300

    Update group-demote.js

--- plugins/group-demote.js
+++ plugins/group-demote.js
@@ -1,35 +1,39 @@
-let handler = async (m, { conn,usedPrefix, command, text }) => {
-if(isNaN(text) && !text.match(/@/g)){
-	
-}else if(isNaN(text)) {
-var number = text.split`@`[1]
-}else if(!isNaN(text)) {
-var number = text
-}
-if(!text && !m.quoted) return conn.reply(m.chat, `منشن شخص`, m)
-if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `الرقم غلط`, m)
-  
-try {
-if(text) {
-var user = number + '@s.whatsapp.net'
-} else if(m.quoted.sender) {
-var user = m.quoted.sender
-} else if(m.mentionedJid) {
-var user = number + '@s.whatsapp.net'
-} 
-} catch (e) {
-} finally {
-conn.groupParticipantsUpdate(m.chat, [user], 'demote')
-m.reply(`✅ *تمت*`)
-}
+const handler = async (m, {conn, usedPrefix, text}) => {
+  if (isNaN(text) && !text.match(/@/g)) {
+    // لا شيء هنا
+  } else if (isNaN(text)) {
+    var number = text.split`@`[1];
+  } else if (!isNaN(text)) {
+    var number = text;
+  }
 
-}
-handler.help = ['demote (@tag)']
-handler.tags = ['group']
-handler.command = ['خفض', 'تخفيض'] 
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-handler.fail = null
+  if (!text && !m.quoted) return conn.reply(m.chat, 
+    `منشن شخص`, m);
 
-export default handler
+  if (number.length > 13 || (number.length < 11 && number.length > 0)) 
+    return conn.reply(m.chat, '❌ رقم الهاتف غير صحيح!', m);
+
+  try {
+    if (text) {
+      var user = number + '@s.whatsapp.net';
+    } else if (await m?.quoted?.sender) {
+      var user = await m?.quoted?.sender;
+    } else if (m.mentionedJid) {
+      var user = number + '@s.whatsapp.net';
+    }
+    
+    await conn.groupParticipantsUpdate(m.chat, [user], 'demote');
+    conn.reply(m.chat, '✅ *تمت*', m);
+  } catch (e) {
+    conn.reply(m.chat, '❌ فشل في تخفيض العضو، قد لا أملك الصلاحيات الكافية', m);
+  }
+};
+
+handler.help = ['demote'].map((v) => 'mention ' + v);
+handler.tags = ['group'];
+handler.command = /^(promote|تخفيض|خفض)$/i;
+handler.group = true;
+handler.admin = true;
+handler.botAdmin = true;
+handler.fail = null;
+export default handler;
