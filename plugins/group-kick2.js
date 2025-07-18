4bea691df1df49935918943ecea7677aad264e02
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-07-18 20:35:49 +0300

    Update group-kick2.js

--- plugins/group-kick2.js
+++ plugins/group-kick2.js
@@ -1,25 +1,35 @@
-let handler = async (m, { conn, participants, usedPrefix, command }) => {
-    let botOwnerJid = '966560801636@s.whatsapp.net'; // Bot owner's JID (replace with the actual one)
-    let kickte = `*مــنشـن الـشـخص !*`;
-
-    if (!m.mentionedJid[0] && !m.quoted) 
-        return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte) });
-
-    let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-
-    if (user === botOwnerJid) {
-        return m.reply(`*تبيني اطرد ابوي!*`);
-    }
-
+const handler = async (m, {conn, participants, usedPrefix, command}) => {  
+  const kicktext = `*مــنشـن الـشـخص !*`;
+  
+  if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
+  
+  // رقم المالك
+  const ownerNumber = "966560801636@s.whatsapp.net";
+  
+  const user = m.mentionedJid[0] ? m.mentionedJid[0] : await m?.quoted?.sender;
+  
+  // التحقق إذا كان المستخدم هو المالك
+  if (user === ownerNumber) {
+    return m.reply("*تبيني اطرد ابوي!*");
+  }
+  
+  // التحقق إذا كان المستخدم هو البوت نفسه
+  if (user === conn.user.jid) {
+    return;
+  }
+  
+  try {
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
-    m.reply(`*تـــم الــطرد !*`);
+    m.reply("*تـــم الــطرد !*");
+  } catch (error) {
+    m.reply("❌ فشل في طرد العضو، قد لا أملك الصلاحيات الكافية");
+  }
 };
 
-handler.help = ['kick @user'];
 handler.tags = ['group'];
+handler.help = ['kick2'];
 handler.command = ['اتوكل', 'كلبطه', 'اخرس', 'طرد', 'دزمها', 'احبك', 'اكرهك', 'مغربي', 'منغولي', 'يمني'];
 handler.admin = true;
 handler.group = true;
 handler.botAdmin = true;
-
 export default handler;
