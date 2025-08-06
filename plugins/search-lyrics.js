a88f0e0cf6f8263cc64fafdf59e182edc3a6dcdc
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-07 01:39:36 +0300

    Update search-lyrics.js

--- plugins/search-lyrics.js
+++ plugins/search-lyrics.js
@@ -72,7 +72,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
 };
 handler.help = ["lirik", "letra"].map((v) => v + " <Apa>");
 handler.tags = ["internet"];
-handler.command = /^(lirik|lyrics|lyric|letra)$/i;
+handler.command = /^(lirik|كلمات-اغنيه|lyric|letra)$/i;
 export default handler;
 
 /* Creditos: https://github.com/darlyn1234 */
