c568fc0cba8959d004fe9e3054720cecc4883a67
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-06 20:43:59 +0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,15 +1,11 @@
-
 const handler = async (m) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_unbanchat
-
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply(tradutor.texto1);
+  m.reply('✅ تم تشغيل البوت.');
 };
-handler.help = ['unbanchat'];
+
+handler.help = ['banchat'];
 handler.tags = ['owner'];
-handler.command = /^unbanchat$/i;
+handler.command = /^تشغيل$/i;
 handler.rowner = true;
+
 export default handler;
