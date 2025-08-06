f7474b05fdc57feeecb65d98efe46dd70d84c5f1
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-07 01:33:33 +0300

    Update maker-ytcomment.js

--- plugins/maker-ytcomment.js
+++ plugins/maker-ytcomment.js
@@ -1,19 +1,12 @@
-
-
-const handler = async (m, {conn, text}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.maker_ytcomment
-
-  if (!text) throw 'No Text';
-  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/youtube-comment', {
-    avatar: await conn.profilePictureUrl(m.sender, 'image').catch((_) => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
-    comment: text,
-    username: conn.getName(m.sender),
-  }), 'error.png', tradutor.texto1, m);
-};
-handler.help = ['ytcomment <comment>'];
-handler.tags = ['maker'];
-handler.command = /^(ytcomment)$/i;
-export default handler;
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'اين النص'
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
+  avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+  comment: text,
+  username: conn.getName(m.sender)
+  }), 'error.png', '*شكرا لتعليق*', m)
+  }
+  handler.help = ['ytcomment <comment>']
+  handler.tags = ['maker'] 
+  handler.command = /^(كومنت)$/i
+  export default handler
