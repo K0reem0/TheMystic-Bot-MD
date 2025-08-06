bd0bcd6f2dbea6011254a656a1dbb495a9509bed
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-06 20:37:52 +0300

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,20 +1,22 @@
+const handler = async (m, { conn, participants, usedPrefix, command }) => {
+  const BANtext = `*منهو الي تبي تمنعه من البوت*`;
+  
+  if (!m.mentionedJid?.[0] && !m.quoted) {
+    return m.reply(BANtext, m.chat, { mentions: conn.parseMention(BANtext) });
+  }
 
-
-const handler = async (m, {conn, participants, usedPrefix, command}) => {
-  const datas = global
-  const idioma = datas.db.data.users[m.sender].language || global.defaultLenguaje
-  const _translate = JSON.parse(fs.readFileSync(`./src/languages/${idioma}.json`))
-  const tradutor = _translate.plugins.owner_banuser
-
-  const BANtext = `${tradutor.texto1}\n*${usedPrefix + command} @${global.suittag}*`;
-  if (!m.mentionedJid[0] && !m.quoted) return m.reply(BANtext, m.chat, {mentions: conn.parseMention(BANtext)});
   let who;
-  if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : await m?.quoted?.sender;
-  else who = m.chat;
+  if (m.isGroup) {
+    who = m.mentionedJid?.[0] ? m.mentionedJid[0] : await m?.quoted?.sender;
+  } else {
+    who = m.chat;
+  }
+
   const users = global.db.data.users;
   users[who].banned = true;
-  m.reply(tradutor.texto2);
+  m.reply('✅ تمت.');
 };
+
 handler.command = /^بان$/i;
 handler.rowner = true;
 export default handler;
