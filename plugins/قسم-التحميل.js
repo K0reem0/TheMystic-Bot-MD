7ceb37cf73bedd643de7a020cd0f725cd13f631d
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-07 22:31:43 +0300

    Create قسم-التحميل.js

---
+++ plugins/قسم-التحميل.js
@@ -0,0 +1,42 @@
+import fs from 'fs'
+
+let handler = async (m, { conn }) => {
+    let menu = `
+*❃ ────────⊰ ❀ ⊱──────── ❃*
+                         *الـتـحـمـيـل*
+*❃ ────────⊰ ❀ ⊱──────── ❃* 
+
+> *◍ انستا*
+> *◍ خلفيات*
+> *◍ فيسبوك*
+> *◍ تيك*
+> *◍ شغل*
+> *◍ تويتر*
+> *◍ مانهوا*
+> *◍ انمي*
+> *◍ بحث*
+> *◍ تطبيق*
+> *◍ صورة*
+
+*❃ ────────⊰ ❀ ⊱──────── ❃*`
+
+    let imgUrl = 'https://files.catbox.moe/yjj0x6.jpg'
+
+    try {
+        await conn.sendMessage(m.chat, { 
+            image: { url: imgUrl }, 
+            caption: menu 
+        }, { quoted: m })
+
+        console.log('Image sent successfully')
+    } catch (e) {
+        console.error(e)
+        conn.reply(m.chat, '❌ Failed to send image', m)
+    }
+}
+
+handler.help = ['main']
+handler.tags = ['group']
+handler.command = /^(قسم-التحميل)$/i 
+
+export default handler
