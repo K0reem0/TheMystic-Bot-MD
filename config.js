da9e062f4e89b4d3989079c5be980cc159cd35f1
عبدالكريم معاذ محمد علي البكاري <202470349@su.edu.ye>
2025-08-11 22:37:55 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ import {fileURLToPath} from 'url';
 import fs from 'fs'; 
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "967779416950"
 global.confirmCode = ""
 global.authFile = `MysticSession`;
 
